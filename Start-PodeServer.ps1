143ad3d920fe9684ae281d170d4d35796b564ece
Martin Walther <it@martin-walther.ch>
2023-03-28 21:05:16 +0200

    Initial upload

---
+++ Start-PodeServer.ps1
@@ -0,0 +1,56 @@
+﻿Import-Module Pode.Web
+
+Start-PodeServer {
+
+    # Enable Loggin to Terminal
+    New-PodeLoggingMethod -Path .\logs -Name "PodeWebServer.log" | Enable-PodeErrorLogging
+    #New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
+
+    # Use default system Theme
+    Use-PodeWebTemplates -Title "Tinu's PSPodeAI" -Theme Dark
+
+    # Add Navbar
+    $Properties = @{
+        Name = 'PowerShellAI on GitHub'
+        Url  = 'https://github.com/dfinke/PowerShellAI'
+        Icon = 'help-circle'
+    }
+    $navgithub = New-PodeWebNavLink @Properties -NewTab
+    Set-PodeWebNavDefault -Items $navgithub
+    
+    # Running on Windows
+    if(($PSVersionTable.PSVersion.Major -lt 6) -or ($IsWindows)){
+        Write-Host "Running on Windows $($PSScriptRoot)"
+        # Add dynamic pages
+        foreach($item in (Get-ChildItem (Join-Path $PSScriptRoot -ChildPath 'pages'))){
+            . "$($item.FullName)"
+        }
+        # Add Listener to Tcp Port 8080 on localhost
+        $EPProperties = @{
+            Address  = 'localhost'
+            Port     = 8090
+            Protocol = 'http'
+        }
+        Add-PodeEndpoint @EPProperties #-SelfSigned
+        # Start Browser
+        $Path = "microsoft-edge:$($EPProperties.Protocol)://$($EPProperties.Address):$($EPProperties.Port)/"
+        Start-Process $Path -WindowStyle maximized       
+    }else{
+        Write-Host "Running on Linux $($PSScriptRoot)"
+        # Add dynamic pages
+        foreach($item in (Get-ChildItem (Join-Path $PSScriptRoot -ChildPath 'pages') -Exclude 'win_*')){
+            . "$($item.FullName)"
+        }
+        # Add Listener to Tcp Port 8080 on localhost
+        $EPProperties = @{
+            Address  = 'localhost'
+            Port     = 8090
+            Protocol = 'http'
+        }
+        Add-PodeEndpoint @EPProperties
+        # Start Browser
+        $Path = "$($EPProperties.Protocol)://$($EPProperties.Address):$($EPProperties.Port)/"
+        Start-Process $Path
+    }
+
+}
---
+++ pages/home-page.ps1
@@ -0,0 +1,58 @@
+﻿Set-PodeWebHomePage -Title  "ChatGPT with PowerShell" -DisplayName 'Jira' -Layouts @(
+
+    New-PodeWebCard -Name "Hey PowerShellAI, let's talk about Jira" -Content @(
+    #New-PodeWebHero -Title 'Welcome!' -Message 'This is the PSPodeAI home page' -Content @(
+
+        New-PodeWebForm -Name 'Jira' -ScriptBlock {
+            Import-Module PowerShellAI
+            $env:OpenAIKey = $($WebEvent.Data.OpenAIKey)
+            $data = Get-GPT3Completion -max_tokens 500 -Prompt "$($WebEvent.Data.Question) $($WebEvent.Data.Summary)"
+            $data.Trim() | Out-PodeWebTextbox -Multiline
+        } -Content @(
+            $Properties = @{
+                Name        = 'OpenAIKey'
+                DisplayName = 'OpenAIKey'
+                Type        = 'Password'
+                Required    = $true
+            }
+            New-PodeWebTextbox @Properties -AutoFocus
+
+            $Properties = @{
+                Name        = 'Question'
+                DisplayName = 'Question'
+                Type        = 'Text'
+                Value       = 'Generate an agile story description as an engineer with 5 acceptance criterias and 2 deliverable objects for'
+                Required    = $true
+            }
+            New-PodeWebTextbox @Properties
+            
+            $Properties = @{
+                Name        = 'Summary'
+                DisplayName = 'Summary'
+                Type        = 'Text'
+                Value       = 'the usage of open ai to generate jira stories'
+                Required    = $true
+            }
+            New-PodeWebTextbox @Properties
+        )
+
+    )
+
+    <#
+    New-PodeWebHero -Title 'Welcome!' -Message 'This is the PSPodeAI home page' -Content @(
+        New-PodeWebText -Value 'Start pode server: Start-PodeServer.ps1' -InParagraph
+        New-PodeWebText -Value 'Restart pode server: Ctrl. + R' -InParagraph
+        New-PodeWebText -Value 'Stop pode server: Ctrl. + C' -InParagraph
+        New-PodeWebText -Value 'All Pages are locatad in ./PodeWeb/pages/' -InParagraph
+    )
+    #>
+
+    <#
+    Import-Module PowerShellAI
+    $env:OpenAIKey = ']xn7M9TmcFeNy##hf(AJ9'
+    $summary
+    $Question = "Generate an agile story description as an engineer with no acceptance criteria for $($summary)"
+    Get-GPT3Completion -max_tokens 500 -Prompt $Question
+    #>
+
+)
